8309c50992c7032a99540de0ddce50ff01709a8f
root <root@root.com>
2020-08-14 22:02:08 +0000

    day 5

--- _03_if_else/_5_circle_calculator/circle_calculator.py
+++ _03_if_else/_5_circle_calculator/circle_calculator.py
@@ -5,7 +5,15 @@ window.withdraw()
 import math
 
 # Next, ask the user if they would like to calculate the area or circumference of a circle.
+like=simpledialog.askstring("","Would you like to calculate the area or circumference of a circle")
+radius=simpledialog.askinteger("","Enter a radius")
 # If they choose area, display the area of the circle using the radius.
+if  like == "area":
+    area = math.pi * radius * radius
+    messagebox.showinfo("",area)
+else:
+    cir=math.pi*radius*2
+    messagebox.showinfo("",cir)
 # Otherwise, display the circumference of the circle using the radius.
 
 #Area = πr^2
--- _03_if_else/_5_shape_selector/shape_selector.py
+++ _03_if_else/_5_shape_selector/shape_selector.py
@@ -9,11 +9,41 @@ if __name__ == '__main__':
     window.withdraw()
     
     # Make a new turtle
-    
+    liz=turtle.Turtle()
+    liz.setheading(0)
+    liz.width(10)
     # Ask the user what shape they want to draw and store it in a variable
-    
+    shape=simpledialog.askstring("","what shape do you want to draw?")
     # Draw the shape requested by the user using if-elif-else statements
-    
+    if shape=="square":
+        liz.color('thistle')
+        for i in range(4):
+            liz.forward(100)
+            liz.right(90)
+    elif shape=="tringle":
+        liz.color("powderblue")
+        for i in range(3):
+            liz.forward(200)
+            liz.left(120)
+    elif shape=="pentagon":
+        liz.color('pink')
+        for i in range(5):
+            liz.forward(100)
+            liz.left (360/5)
+    elif shape=="rectangle":
+        liz.color('lightgreen')
+        for i in range(2):
+            liz.forward(100)
+            liz.right(90)
+            liz.forward(60)
+            liz.right(90)
+    elif shape=="rhombus":
+        liz.color('cornflowerblue')
+        for i in range(4):
+            liz.forward(100)
+            liz.left(120)
+            liz.forward(100)
+            liz.right(120)
     # Call the turtle .done() method
-    
+    turtle.done()
     window.mainloop()
